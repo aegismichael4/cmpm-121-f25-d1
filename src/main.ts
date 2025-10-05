0daa2b9d071d7373c7fbc9311f28944405d2a50e
aegismichael4 <aegismichael4@gmail.com>
2025-10-05 03:49:06 +0000

    read the instructions properly and made my button an emoji (stage 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,26 @@ import "./style.css";
 let counter: number = 0;
 let autoClickSpeed: number = 2000;
 
+const boxSize = 50;
+
 let autoClickInterval = setInterval(incrementTotal, autoClickSpeed, 1);
+clearInterval(autoClickInterval);
 
 //<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 
 document.body.innerHTML = `
   <h1>Game Title</h1>
-  <p>Counter: <span id="counter">0</span></p>
-  <button id="increment">Button</button>
+  <p>Progress... <span id="counter">0</span></p>
+  <button id="increment">&#128230;</button>
+  <p>\n\n</p>
   <button id="enableAutoClick">Enable Auto Click</button>
 `;
-
-clearInterval(autoClickInterval);
 
 const counterElement = document.getElementById("counter")!;
 const increment = document.getElementById("increment")!;
 const autoClick = document.getElementById("enableAutoClick")!;
+
+setBoxSize(boxSize);
 
 increment.addEventListener("click", () => {
   incrementTotal(1);
@@ -30,7 +34,13 @@ autoClick.addEventListener("click", () => {
   autoClickInterval = setInterval(incrementTotal, autoClickSpeed, 1);
 });
 
-function incrementTotal(amount: number) {
-  counter += amount;
+function incrementTotal(amountToAdd: number) {
+  counter += amountToAdd;
   counterElement.innerHTML = counter.toString();
+}
+
+function setBoxSize(size: number) {
+  const newSizeString: string = "border: none; background: none; font-size: " +
+    size.toString() + "px";
+  increment.setAttribute("style", newSizeString);
 }
